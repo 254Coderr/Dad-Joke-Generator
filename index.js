14d66b577249107c030018cdb35fdfa0bad72750
Julius Magomere <makojulius@gmail.com>
2023-11-08 15:56:35 +0300

    added more Javascript elements like buttun and event listeners to the file

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-const btnElement=document.getElementById("btn");
+// Get references to the button plus joke display elements in the HTML
+const btnElement = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+ // Define the API key for the dad jokes API (assuming it's a placeholder for a real key)
+ const apiKey = "YVguAG6KmqXoRqPZhaOMRQ==KQIqDVyCj3wTYZx5";
+ // Define the options for the API request, included the API key in the headers
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-key":"YVguAG6KmqXoRqPZhaOMRQ==KQIqDVyCj3wTYZx5",
+    },
+};
+
+// Define the URL of the dad jokes API
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes";
+
+// Define an asynchronous function to fetch and display a dad joke
+async function getJoke() {
+    try {
+        // Update the joke display with a loading message
+        jokeEl.innerText = "Updating...";
+        // Disable the button during the request
+        btnElement.disabled = true;
+        btnElement.innerText = "Loading...";
+
+        // Fetch a dad joke from the API using the specified options
+        const response = await fetch(apiUrl, options);
+
+        if (response.ok) {
+            // If the response is successful, parse the JSON data
+            const data = await response.json();
+
+            // Enable the button and update the joke display with the retrieved joke
+            btnElement.disabled = false;
+            btnElement.innerText = "Tell me a joke";
+            jokeEl.innerText = data[0].joke;
+        } else {
+            // If the response is not successful, throw an error
+            throw new Error("Response not OK");
+        }
+        // Handle any errors that come during the fetch operation
+    } catch (error) {
+        
+        // Update the joke display with an error message
+        jokeEl.innerText = "An error happened, try again later!";
+        // Enable the button again
+        btnElement.disabled = false;
+        btnElement.innerText = "Tell me a joke";
+    }
+}
+
+// Add a click event listener to the button element to activate the getJoke function
+btnElement.addEventListener("click", getJoke);
